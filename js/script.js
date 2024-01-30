6792d8067e80185120a128fc13d82a5d7380b2ee
Almansur342 <abdullahalmansur2002@gmail.com>
2024-01-30 11:13:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('button').addEventListener('click', function(){
+  // get email
+  const emailField = document.getElementById('email');
+  const userEmail = emailField.value;
+  // get password
+  const passwordField = document.getElementById('password');
+  const userPassword = passwordField.value;
+  if(userEmail == 'abdullah@gmail.com' && userPassword == 'secret'){
+   window.location.href = 'banking.html'
+  }
+})
